8386048595ea5e9f77a19394ef85daaede0daab1
markmark345 <natapatchara.anuroje@gmail.com>
2022-09-29 19:11:43 +0700

    Fix css navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -6,8 +6,8 @@ interface INavbar {}
 const Navbar: FC<INavbar> = () => {
   return (
     <div
-      className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b 
-      lg:border-slate-400/10 shadow-lg bg-white/95 "
+      className="sticky top-0  w-full backdrop-blur flex-none transition-colors duration-500  lg:border-b 
+      lg:border-transparent shadow-sm bg-transparent"
     >
       <div className="max-w-8xl mx-auto lg:container">
         <div className="py-6 border-b border-slate-900/10 lg:px-20 lg:border-0  mx-4 lg:mx-0">
